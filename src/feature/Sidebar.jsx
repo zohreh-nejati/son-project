99f88fa413443ff012c44a60c1b1afabe1054c87
zohreh <zohrehnejati0@email.com>
2025-09-29 17:10:37 +0330

    WIP: work in close sidebar

--- src/feature/Sidebar.jsx
+++ src/feature/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
-import { useRef, useState } from "react";
+// import { useRef, useState } from "react";
 import "./Sidebar.css";
 
 const sidebardData = [
@@ -15,70 +15,34 @@ const sidebardData = [
   { title: "داشبوردها", icon: "/icons/sidebar/Dashboard-Disable.svg" },
   { title: "مستندات", icon: "/icons/sidebar/Documents-Disable.svg" },
   { title: "ماژول‌های مدیریت مالی", type: "title" },
-  { title: "مستندات", icon: "/icons/sidebar/Documents-Disable.svg" },
+  {
+    title: "گزارشات و صورت‌های مالی",
+    icon: "/icons/sidebar/Bar-Chart-Disable.svg",
+  },
+  { title: "حسابداری هزینه‌ها", icon: "/icons/sidebar/Money-Bill-Disable.svg" },
+  {
+    title: "تجمیع و بستن حساب‌ها",
+    icon: "/icons/sidebar/User-Account-Disable.svg",
+  },
+  { title: "تلفیق", icon: "/icons/sidebar/Shape-union-Disablee.svg" },
+  { title: "مجامع", icon: "/icons/sidebar/Collection-Disable.svg" },
+  { title: "ماژول‌های مدیریت استراتژی", type: "title" },
+  {
+    title: "پیش‌بینی و برنامه‌ریزی",
+    icon: "/icons/sidebar/Note-Disable.svg",
+  },
+  { title: "بودجه‌ریزی", icon: "/icons/sidebar/Wallet-Disable.svg" },
+  {
+    title: "کارت امتیازی متوازن",
+    icon: "/icons/sidebar/Documents-Disable.svg",
+  },
+  {
+    title: "کارت امتیازی متوازن فردی",
+    icon: "/icons/sidebar/Credit-Card-Disable.svg",
+  },
+  { title: "سایر ماژول‌ها", type: "title" },
+  { title: "حسابرسی", icon: "/icons/sidebar/Contact-Card-Disable.svg" },
 ];
-
-function Sidebar() {
-  const [isOpen] = useState();
-  const dockBar = useRef(null);
-
-  // Toggle(Open/Close) the Sidebar
-  function toggleClick() {
-    dockBar.current.toggle();
-    // setIsOpen(dockBar.current.isOpen);
-  }
-
-  return (
-    <div className="controlSidebarComponent">
-      <SidebarComponent
-        id="dockSidebar"
-        ref={dockBar}
-        enableDock={true}
-        type="Push"
-        dockSize="72px"
-        width="255px"
-        position="Right"
-        isOpen={isOpen}
-      >
-        <div className="dock">
-          <div className="sidebarHeader">
-            {isOpen && (
-              <img
-                src="/logo/Logo.svg"
-                alt="logo icon"
-                className="sidebarLogo"
-              ></img>
-            )}
-            <img
-              src="/icons/sidebar/Double-arrow-Right-Disable.svg"
-              alt="arrow icon"
-              className="sidebarArrow"
-              onClick={toggleClick}
-            ></img>
-          </div>
-
-          <ul className="SidebarList">
-            {sidebardData.map((list) => (
-              <li key={list.title} className={list.type && "listTitle"}>
-                {list.type ? (
-                  ""
-                ) : (
-                  <img
-                    src={list.icon}
-                    className="e-icons"
-                    alt="notif icon"
-                  ></img>
-                )}
-                {/* {isOpen && <h1 className="e-text">{list.title}</h1>} */}
-                <h1 className="e-text">{list.title}</h1>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </SidebarComponent>
-    </div>
-  );
-}
 
 // export default Sidebar;
 
@@ -97,7 +61,7 @@ function App() {
         ref={(Sidebar) => (dockBar = Sidebar)}
         enableDock={true}
         dockSize="72px"
-        width="220px"
+        width="255px"
         type="Push"
       >
         <div className="dock">
