7ec494d2fd892088ee9ed61a6868459971294202
zohreh <zohrehnejati0@email.com>
2025-09-29 16:22:49 +0330

    WIP: adding syncfusion style

--- src/feature/Sidebar.jsx
+++ src/feature/Sidebar.jsx
@@ -1,7 +1,9 @@
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
-import styles from "./Sidebar.module.css";
+import { useRef, useState } from "react";
+import "./Sidebar.css";
 
 const sidebardData = [
+  { title: "ماژول‌های پایه", type: "title" },
   { title: "اعلان‌ها", icon: "/icons/sidebar/Notificatins-Disable.svg" },
   { title: "وظایف", icon: "/icons/sidebar/Duties-Disable.svg" },
   { title: "تکمیل آمار", icon: "/icons/sidebar/statistic-Disable.svg" },
@@ -12,34 +14,63 @@ const sidebardData = [
   { title: "گزارش‌ها", icon: "/icons/sidebar/Reports-Disable.svg" },
   { title: "داشبوردها", icon: "/icons/sidebar/Dashboard-Disable.svg" },
   { title: "مستندات", icon: "/icons/sidebar/Documents-Disable.svg" },
+  { title: "ماژول‌های مدیریت مالی", type: "title" },
+  { title: "مستندات", icon: "/icons/sidebar/Documents-Disable.svg" },
 ];
 
 function Sidebar() {
-  let dockBar;
+  const [isOpen] = useState();
+  const dockBar = useRef(null);
+
   // Toggle(Open/Close) the Sidebar
   function toggleClick() {
-    dockBar.toggle();
+    dockBar.current.toggle();
+    // setIsOpen(dockBar.current.isOpen);
   }
+
   return (
-    <div className="control-section">
-      {/* Initializing the Sidebar component */}
+    <div className="controlSidebarComponent">
       <SidebarComponent
         id="dockSidebar"
-        ref={(Sidebar) => (dockBar = Sidebar)}
+        ref={dockBar}
         enableDock={true}
         type="Push"
         dockSize="72px"
         width="255px"
         position="Right"
+        isOpen={isOpen}
       >
         <div className="dock">
-          <ul className={styles.SidebarList}>
+          <div className="sidebarHeader">
+            {isOpen && (
+              <img
+                src="/logo/Logo.svg"
+                alt="logo icon"
+                className="sidebarLogo"
+              ></img>
+            )}
+            <img
+              src="/icons/sidebar/Double-arrow-Right-Disable.svg"
+              alt="arrow icon"
+              className="sidebarArrow"
+              onClick={toggleClick}
+            ></img>
+          </div>
+
+          <ul className="SidebarList">
             {sidebardData.map((list) => (
-              <li key={list.title} onClick={toggleClick}>
-                <img src={list.icon} alt="notif icon"></img>
-                <h1 className="e-text" title="menu">
-                  {list.title}
-                </h1>
+              <li key={list.title} className={list.type && "listTitle"}>
+                {list.type ? (
+                  ""
+                ) : (
+                  <img
+                    src={list.icon}
+                    className="e-icons"
+                    alt="notif icon"
+                  ></img>
+                )}
+                {/* {isOpen && <h1 className="e-text">{list.title}</h1>} */}
+                <h1 className="e-text">{list.title}</h1>
               </li>
             ))}
           </ul>
@@ -49,4 +80,97 @@ function Sidebar() {
   );
 }
 
-export default Sidebar;
+// export default Sidebar;
+
+function App() {
+  let dockBar;
+  // Toggle(Open/Close) the Sidebar
+  function toggleClick() {
+    dockBar.toggle();
+  }
+
+  return (
+    <div className="control-section">
+      {/* Initializing the Sidebar component */}
+      <SidebarComponent
+        id="dockSidebar"
+        ref={(Sidebar) => (dockBar = Sidebar)}
+        enableDock={true}
+        dockSize="72px"
+        width="220px"
+        type="Push"
+      >
+        <div className="dock">
+          <div className="sidebarHeader">
+            <span className="e-text">
+              <img
+                src="/logo/Logo.svg"
+                alt="logo icon"
+                className="sidebarLogo"
+              ></img>
+            </span>
+            <span className="e-icon">
+              <img
+                src="/icons/sidebar/Double-arrow-Right-Disable.svg"
+                alt="arrow icon e-icons"
+                className="sidebarArrow"
+                onClick={toggleClick}
+              ></img>
+            </span>
+          </div>
+          <ul>
+            <ul className="SidebarList" onClick={toggleClick}>
+              {sidebardData.map((list) => (
+                <li key={list.title} className={list.type && "listTitle"}>
+                  {list.type ? (
+                    ""
+                  ) : (
+                    <img
+                      src={list.icon}
+                      className="e-icons"
+                      alt="notif icon"
+                    ></img>
+                  )}
+                  {/* {isOpen && <h1 className="e-text">{list.title}</h1>} */}
+                  <span className="e-text">{list.title}</span>
+                </li>
+              ))}
+            </ul>
+
+            {/* <li className="sidebar-item" id="toggle" onClick={toggleClick}>
+              <span className="e-icons expand" />
+              <span className="e-text" title="menu">
+                Menu
+              </span>
+            </li>
+            <li className="sidebar-item">
+              <span className="e-icons home" />
+              <span className="e-text" title="home">
+                Home
+              </span>
+            </li>
+            <li className="sidebar-item">
+              <span className="e-icons profile" />
+              <span className="e-text" title="profile">
+                Profile
+              </span>
+            </li>
+            <li className="sidebar-item">
+              <span className="e-icons info" />
+              <span className="e-text" title="info">
+                Info
+              </span>
+            </li>
+            <li className="sidebar-item">
+              <span className="e-icons settings" />
+              <span className="e-text" title="settings">
+                Settings
+              </span>
+            </li> */}
+          </ul>
+        </div>
+      </SidebarComponent>
+    </div>
+  );
+}
+export default App;
