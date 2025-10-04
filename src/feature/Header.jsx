8579330432fe246cc1e0e222fb92229146b1dad2
zohreh <zohrehnejati0@email.com>
2025-10-04 12:46:35 +0330

    feat complete header

--- src/feature/Header.jsx
+++ src/feature/Header.jsx
@@ -4,14 +4,17 @@ import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 const Header = () => {
   return (
     <div className="control-container">
-      <AppBarComponent colorMode="Primary">
-        <ButtonComponent
-          cssClass="e-inherit"
-          iconCss="e-icons e-menu"
-        ></ButtonComponent>
-        <span className="prominent">AppBar Component with Prominent mode</span>
+      <AppBarComponent className="appBar">
         <div className="e-appbar-spacer"></div>
-        <ButtonComponent cssClass="e-inherit">FREE TRIAL</ButtonComponent>
+        <ButtonComponent cssClass="e-inherit">
+          <img src="/header-logo/Duties-Dark.svg" alt="logo icon"></img>
+        </ButtonComponent>
+        <ButtonComponent cssClass="e-inherit">
+          <img src="/header-logo/Notifications-Dark.svg" alt="logo icon"></img>
+        </ButtonComponent>
+        <ButtonComponent cssClass="e-inherit">
+          <img src="/header-logo/User-Account-Dark.svg" alt="logo icon"></img>
+        </ButtonComponent>
       </AppBarComponent>
     </div>
   );
