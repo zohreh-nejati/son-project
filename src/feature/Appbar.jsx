5242a1ab7081ad2b9c633452eb0b7ff58a4a093e
zohreh <zohrehnejati0@email.com>
2025-10-04 16:24:25 +0330

    feat: add secondary header

--- src/feature/Appbar.jsx
+++ src/feature/Appbar.jsx
@@ -7,13 +7,13 @@ const Appbar = () => {
       <AppBarComponent className="appBar">
         <div className="e-appbar-spacer"></div>
         <ButtonComponent cssClass="e-inherit">
-          <img src="/header-logo/Duties-Dark.svg" alt="logo icon"></img>
+          <img src="/icons/header/Duties-Dark.svg" alt="logo icon"></img>
         </ButtonComponent>
         <ButtonComponent cssClass="e-inherit">
-          <img src="/header-logo/Notifications-Dark.svg" alt="logo icon"></img>
+          <img src="/icons/header/Notifications-Dark.svg" alt="logo icon"></img>
         </ButtonComponent>
         <ButtonComponent cssClass="e-inherit">
-          <img src="/header-logo/User-Account-Dark.svg" alt="logo icon"></img>
+          <img src="/icons/header/User-Account-Dark.svg" alt="logo icon"></img>
         </ButtonComponent>
       </AppBarComponent>
     </div>
--- src/feature/MainContentHeader.jsx
+++ src/feature/MainContentHeader.jsx
@@ -1,13 +1,36 @@
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import "./MainContentHeader.css";
 
 export default function MainContentHeader() {
   return (
-    <div className="mainContentHeader">
+    <div className="main_content--Header">
       <div className="text_wrapper">
         <h1 className="active">کاربران و دسترسی ها</h1>
         <h1>نقش ها</h1>
       </div>
       <hr className="divider" />
+      <div>
+        <h1 className="main_content--title">کاربران و دسترسی ها</h1>
+        <div className="sereach_add--wrapper">
+          <div className="serach-wrapper">
+            <input className="search-input" placeholder="جستجو" />
+            <img
+              className="search-icon"
+              alt="search icon"
+              src="icons/header/Search-Disable.svg"
+            ></img>
+          </div>
+
+          <ButtonComponent className="add-button">
+            <img
+              className="search-icon"
+              alt="search icon"
+              src="icons/header/Add-Plus-White.svg"
+            ></img>
+            کاربر جدید
+          </ButtonComponent>
+        </div>
+      </div>
     </div>
   );
 }
